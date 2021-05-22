7dcdab5fa0742332359911e8de1ddf4b837af206
Hiroto Hasegawa <hiroto.hasegawa.0828@gmail.com>
2021-05-22 17:46:17 +0900

    feat: project毎に合計時間を表示するように変更

--- main.py
+++ main.py
@@ -32,7 +32,25 @@ with open(filename, encoding='utf-8-sig') as f:
         records.setdefault(csv_row['Project'], []).append(time_and_duration)
 
 for project, time_and_duration_array in records.items():
-    text = f'- {project}'
+    total_hours, total_minutes, total_seconds = 0, 0, 0
+
+    title_text = ''
     for time_and_duration in time_and_duration_array:
-        text += f'\n  [{time_and_duration["duration"]}]: {time_and_duration["title"]}'
+        title_text += f'\n  -  [{time_and_duration["duration"]}]: {time_and_duration["title"]}'
+
+        hours, minutes, seconds = [
+            int(x) for x in time_and_duration["duration"].split(':')
+        ]
+        total_hours += hours
+        total_minutes += minutes
+        total_seconds += seconds
+
+    additional_minutes, total_seconds = divmod(total_seconds, 60)
+    additional_hours, total_minutes = divmod(
+        total_minutes + additional_minutes, 60
+    )
+    total_hours += additional_hours
+
+    project_text = f'- [{str(total_hours).zfill(2)}:{str(total_minutes).zfill(2)}:{str(total_seconds).zfill(2)}] {project}'
+    text = project_text + title_text
     print(text)
